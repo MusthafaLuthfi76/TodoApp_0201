bac1e9ce6e2243c526c25e5d6bd83dcc41dbbb35
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2025-03-21 00:30:23 +0700

    Inisialisasi variabel

--- lib/todoapp.dart
+++ lib/todoapp.dart
@@ -8,6 +8,11 @@ class FormPage extends StatefulWidget {
 }
 
 class _FormPageState extends State<FormPage> {
+  final TextEditingController nameController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  List<String> tasks = [];
+
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
